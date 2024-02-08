1901247a65f20d0890eb8818db30543729777ea6
THANYAPISIT1 <65025895@up.ac.th>
2024-02-08 23:28:51 +0700

    Submit

---
+++ vue_backendproject/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>Hello</h1>
+<RouterView/>
+</template>
---
+++ vue_backendproject/src/routes/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import Home from '../views/Home.vue'
+// import Contract from '../views/Contract.vue'
+// import AddCon from '../views/AddCon.vue'
+// import About from '../views/About.vue'
+
+const Home = () => import('../views/Home.vue')
+// const Contract = () => import('../views/Contract.vue')
+// const AddCon = () => import('../views/AddCon.vue')
+// const About = () => import('../views/About.vue')
+
+const routes = [
+    { path: '/', component: Home},
+    { path: '/About', component: () => import('../views/About.vue')},
+    { path: '/Addcon', component: () => AddCon('../views/AddCon.vue')},
+    { path: '/Contract', component: () => Contract('../views/Contract.vue')}
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+export default router;
